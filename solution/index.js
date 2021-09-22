bd3d2be5f601c163e003cc1a9b86a8c5ee4f56ed
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-09-22 15:07:18 +0300

    addTask func.
    I added event listener to all 'Add' buttons,and wrote function that will add it to the right list.
    
     *Also added support function - creatElement.

---
+++ solution/index.js
@@ -0,0 +1,30 @@
+function createElement(tagName, children = [], classes = [], attributes = {}, eventListeners = {}) {
+    const el = document.createElement(tagName);
+    // Children
+    for(const child of children) {
+        el.append(child);
+    }
+    // Classes
+    for(const cls of classes) {
+      el.classList.add(cls);
+    }
+    // Attributes
+    for (const attr in attributes) {
+      el.setAttribute(attr, attributes[attr]);
+    }
+    return el;
+}
+
+function addTask({target}){
+    if(target.className==="add-button"){
+        const task = target.previousSibling.previousSibling.value;
+        if(task!==""){
+            const taskEl = createElement("li",[task],["task"])
+            target.closest('ul').append(taskEl);
+            target.previousSibling.previousSibling.value="";
+        }
+    }
+}
+
+document.querySelector("body").addEventListener("click",addTask);
+  
